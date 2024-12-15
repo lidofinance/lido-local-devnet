ec8a130a58b0911831ef4efdbb81b28585c1a43b
Eddort <webisforce@gmail.com>
2024-12-15 15:18:14 +0100

    feat: kurtosis cleanup command description

--- cli/commands/network/down.ts
+++ cli/commands/network/down.ts
@@ -3,12 +3,16 @@ import { kurtosisApi } from "../../lib/kurtosis/index.js";
 import { baseConfig, jsonDb } from "../../config/index.js";
 import fs from "node:fs/promises";
 
-export default class KurtosisUp extends Command {
-  static description = "Check EL node is alive (can process transactions)";
+export default class KurtosisCleanUp extends Command {
+  static description = "Destroys the Kurtosis enclave, cleans the JSON database, and removes network artifacts.";
 
   async run() {
+    this.log("Destroying Kurtosis enclave...");
     await kurtosisApi.destroyEnclave(baseConfig.network.name);
+    this.log("Cleaning JSON database...");
     await jsonDb.clean();
-    await fs.rm(baseConfig.artifacts.paths.network, { recursive: true });
+    this.log("Removing network artifacts...");
+    await fs.rm(baseConfig.artifacts.paths.network, { recursive: true, force: true });
+    this.log("Cleanup completed successfully.");
   }
 }
