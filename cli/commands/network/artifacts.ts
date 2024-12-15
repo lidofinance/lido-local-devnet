b2bb0103f073192a14ab2b4b9d02fb5f976ce9d3
Eddort <webisforce@gmail.com>
2024-12-15 15:16:35 +0100

    refactor: artifacts command description

--- cli/commands/network/artifacts.ts
+++ cli/commands/network/artifacts.ts
@@ -1,36 +1,36 @@
-// el_cl_genesis_data
 import { Command } from "@oclif/core";
 import fs from "node:fs/promises";
 import { baseConfig } from "../../config/index.js";
 import { execa } from "execa";
 import path from "node:path";
+import { CommandError } from "@oclif/core/interfaces";
 
-export default class KurtosisUp extends Command {
-  static description = "Check EL node is alive (can process transactions)";
+export default class DownloadKurtosisArtifacts extends Command {
+  static description = "Downloads the genesis data for EL and CL nodes from the Kurtosis enclave.";
 
   async run() {
-    this.log("Stopping EL and CL Nodes...");
+    this.log("Downloading EL and CL nodes genesis data...");
 
     const networkName = baseConfig.network.name;
 
-    try {
-      await fs.mkdir(baseConfig.artifacts.paths.root, { recursive: true });
-      await execa(
-        "kurtosis",
-        [
-          "files",
-          "download",
-          networkName,
-          "el_cl_genesis_data",
-          path.join(baseConfig.artifacts.paths.root, 'network'),
-        ],
-        {
-          stdio: "inherit",
-        }
-      );
-      this.log("Nodes stopped successfully.");
-    } catch (error) {
-      this.error("Failed to stop nodes. Ensure Docker is running.");
-    }
+    await fs.mkdir(baseConfig.artifacts.paths.root, { recursive: true });
+    await execa(
+      "kurtosis",
+      [
+        "files",
+        "download",
+        networkName,
+        "el_cl_genesis_data",
+        path.join(baseConfig.artifacts.paths.root, 'network'),
+      ],
+      {
+        stdio: "inherit",
+      }
+    );
+    this.log("Genesis data downloaded successfully.");
+  }
+
+  protected catch(err: CommandError): Promise<any> {
+    this.error("Genesis data download error.", err);
   }
 }
