748d2f9c47f3c0ec8db29ca80f66702cf17cf52b
Eddort <webisforce@gmail.com>
2024-12-15 14:29:04 +0100

    fix: network name params

--- cli/commands/network/kurtosis/info.ts
+++ cli/commands/network/kurtosis/info.ts
@@ -1,12 +1,13 @@
 import { Command } from "@oclif/core";
 import { kurtosisApi } from "../../../lib/kurtosis/index.js";
 import { displayUrlTable } from "../../../lib/console/index.js";
+import { baseConfig } from "../../../config/index.js";
 
 export default class KurtosisUp extends Command {
   static description = "Check EL node is alive (can process transactions)";
 
   async run() {
-    const output = await kurtosisApi.getEnclaveInfo("my-testnet");
-    displayUrlTable(output)
+    const output = await kurtosisApi.getEnclaveInfo(baseConfig.network.name);
+    displayUrlTable(output);
   }
 }
