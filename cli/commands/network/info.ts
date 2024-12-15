b550a5f9583625593d67db851b4ebea5ed423b72
Eddort <webisforce@gmail.com>
2024-12-15 15:19:41 +0100

    feat: kurtosis info command description

--- cli/commands/network/info.ts
+++ cli/commands/network/info.ts
@@ -3,10 +3,12 @@ import { kurtosisApi } from "../../lib/kurtosis/index.js";
 import { displayUrlTable } from "../../lib/console/index.js";
 import { baseConfig } from "../../config/index.js";
 
-export default class KurtosisUp extends Command {
-  static description = "Check EL node is alive (can process transactions)";
+export default class KurtosisGetInfo extends Command {
+  static description =
+    "Retrieves and displays information about the Kurtosis enclave.";
 
   async run() {
+    this.log("Retrieving Kurtosis enclave information...");
     const output = await kurtosisApi.getEnclaveInfo(baseConfig.network.name);
     displayUrlTable(output);
   }
