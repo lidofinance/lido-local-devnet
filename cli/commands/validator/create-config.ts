5a4115226326a324db90ce122868c0e416b70f9d
Eddort <webisforce@gmail.com>
2024-12-22 21:40:11 +0100

    feat: new create config

--- cli/commands/validator/create-config.ts
+++ cli/commands/validator/create-config.ts
@@ -1,6 +1,9 @@
-import { Command } from "@oclif/core";
+import { Command, Flags } from "@oclif/core";
 import { baseConfig, jsonDb, validatorsState } from "../../config/index.js";
-import { fetchActiveValidators } from "../../lib/validator/index.js";
+import {
+  fetchActiveValidators,
+  groupByWithdrawalCredentials,
+} from "../../lib/validator/index.js";
 import { generateDockerComposeOneService } from "../../lib/validator/val-teku-one.js";
 import { assert } from "console";
 import {
@@ -8,11 +11,25 @@ import {
   fetchValidatorGraffiti,
 } from "../../lib/keymanager-api/index.js";
 
-export default class VerifyCore extends Command {
-  static description = "Verify deployed lido-core contracts";
-
+export default class CreateValidatorConfig extends Command {
+  static description = "Generation of configuration to run validators";
+  static flags = {
+    wc: Flags.string({
+      char: "w",
+      description: "Custom withdrawal credentials (optional)",
+    }),
+  };
   async run() {
-    console.log(baseConfig.kurtosis.config.participants_matrix.cl[0].cl_image);
+    const { flags } = await this.parse(CreateValidatorConfig);
+    // define WC array (lido and local, for tests)
+    const targetWCs = [
+      "0x010000000000000000000000f93ee4cf8c6c40b329b0c0626f28333c132cf241",
+    ];
+
+    if (flags.wc) {
+      targetWCs.push(flags.wc);
+    }
+
     const VC_IMAGE =
       baseConfig.kurtosis.config.participants_matrix?.cl?.[0]?.cl_image;
     assert(
@@ -34,10 +51,8 @@ export default class VerifyCore extends Command {
     const name = state.network?.binding?.name ?? network.name;
 
     const validators = await fetchActiveValidators(cl);
-    const targetValidators = await validators.data.filter(
-      (v) =>
-        v.validator.withdrawal_credentials ===
-        "0x010000000000000000000000f93ee4cf8c6c40b329b0c0626f28333c132cf241"
+    const targetValidators = await validators.data.filter((v) =>
+      targetWCs.includes(v.validator.withdrawal_credentials)
     );
 
     if (!targetValidators.length) {
@@ -51,16 +66,20 @@ export default class VerifyCore extends Command {
       validatorsApi,
       keystores.data[0].validating_pubkey
     );
+
     const configDir = baseConfig.validator.paths.docker;
 
+    // validators by group
+
+    const valGroups = groupByWithdrawalCredentials(targetValidators);
+
     await generateDockerComposeOneService(configDir, {
-      validatorsInfo: targetValidators,
       keysDir: baseConfig.artifacts.paths.validator,
       clPrivateUrl,
       dockerImage: VC_IMAGE,
       dockerNetwork: `kt-${name}`,
-      validatorFeeRecipient: baseConfig.sharedWallet[0].publicKey,
       graffiti: graffiti.data.graffiti,
+      valGroups,
     });
   }
 }
