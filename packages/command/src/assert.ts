0082d4517bed9054922328942a4ec830f9e9b2d4
Eddort <webisforce@gmail.com>
2025-01-27 23:28:18 +0100

    feat: add fatal method

--- packages/command/src/assert.ts
+++ packages/command/src/assert.ts
@@ -1,5 +1,11 @@
+import { DevNetError } from "./error.js";
+
 export function assert(condition: boolean, message: string): asserts condition {
   if (!condition) {
-    throw new Error(message);
+    throw new DevNetError(message);
   }
+}
+
+export function fatal(message: string): never {
+  throw new DevNetError(message);
 }
