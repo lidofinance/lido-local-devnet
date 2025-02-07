6c7b75b9c29be551cb7855e54801679afc6998b6
Eddort <webisforce@gmail.com>
2025-02-07 14:25:01 +0100

    feat: new up command

--- src/commands/up.ts
+++ src/commands/up.ts
@@ -4,10 +4,19 @@ import { BlockscoutUp } from "./blockscout/up.js";
 import { KurtosisGetInfo } from "./chain/info.js";
 import { KurtosisUp } from "./chain/up.js";
 import { ActivateCSM } from "./csm/activate.js";
+import { LidoAddCSMOperatorWithKeys } from "./csm/add-operator.js";
+import { DeployCSVerifier } from "./csm/add-verifier.js";
 import { DeployCSMContracts } from "./csm/deploy.js";
 import { ActivateLidoProtocol } from "./lido-core/activate.js";
+import { LidoAddKeys } from "./lido-core/add-keys.js";
+import { LidoAddOperator } from "./lido-core/add-operator.js";
 import { DeployLidoContracts } from "./lido-core/deploy.js";
+import { LidoDeposit } from "./lido-core/deposit.js";
+import { GenerateLidoDevNetKeys } from "./lido-core/keys/generate.js";
+import { UseLidoDevNetKeys } from "./lido-core/keys/use.js";
 import { ReplaceDSM } from "./lido-core/replace-dsm.js";
+import { LidoSetStakingLimit } from "./lido-core/set-staking-limit.js";
+import { ValidatorAdd } from "./validator/add.js";
 
 export const DevNetUp = command.cli({
   description:
@@ -48,58 +57,47 @@ export const DevNetUp = command.cli({
       logger.log("Replaces the DSM with an EOA.");
       await dre.runCommand(ReplaceDSM, {});
 
-      // const NOR_DEVNET_OPERATOR = "devnet_nor_1";
-      // const CSM_DEVNET_OPERATOR = "devnet_csm_1";
+      const NOR_DEVNET_OPERATOR = "devnet_nor_1";
+      const CSM_DEVNET_OPERATOR = "devnet_csm_1";
 
-      // logger.log("Generate keys for NOR Module.");
-      // await dre.runCommand("lido:keys:generate");
-      // logger.log("Allocate keys for NOR Module.");
-      // await dre.runCommand("lido:keys:use", ["--name", NOR_DEVNET_OPERATOR]);
+      logger.log("Generate keys for NOR Module.");
+      await dre.runCommand(GenerateLidoDevNetKeys, {});
+      logger.log("Allocate keys for NOR Module.");
+      await dre.runCommand(UseLidoDevNetKeys, { name: NOR_DEVNET_OPERATOR });
 
-      // logger.log("Generate keys for CSM Module.");
-      // await dre.runCommand("lido:keys:generate");
-      // logger.log("Allocate keys for CSM Module.");
-      // await dre.runCommand("lido:keys:use", ["--name", CSM_DEVNET_OPERATOR]);
+      logger.log("Generate keys for CSM Module.");
+      await dre.runCommand(GenerateLidoDevNetKeys, {});
+      logger.log("Allocate keys for CSM Module.");
+      await dre.runCommand(UseLidoDevNetKeys, { name: CSM_DEVNET_OPERATOR });
 
-      // logger.log("Add NOR operator.");
-      // await dre.runCommand("onchain:lido:add-operator", [
-      //   "--name",
-      //   NOR_DEVNET_OPERATOR,
-      // ]);
-      // logger.log(`Operator ${NOR_DEVNET_OPERATOR} added`);
+      logger.log("Add NOR operator.");
+      await dre.runCommand(LidoAddOperator, { name: NOR_DEVNET_OPERATOR });
+      logger.log(`Operator ${NOR_DEVNET_OPERATOR} added`);
 
-      // logger.log(`Increase staking limit for NOR`);
-      // await dre.runCommand("onchain:lido:set-staking-limit", [
-      //   "--operatorId",
-      //   "0",
-      //   "--limit",
-      //   "30",
-      // ]);
+      logger.log("Add NOR keys.");
+      await dre.runCommand(LidoAddKeys, { name: NOR_DEVNET_OPERATOR, id: 0 });
+      logger.log(`Keys for operator ${NOR_DEVNET_OPERATOR} added`);
 
-      // logger.log("Add NOR keys.");
-      // await dre.runCommand("onchain:lido:add-keys", [
-      //   "--name",
-      //   NOR_DEVNET_OPERATOR,
-      //   "--id",
-      //   "0",
-      // ]);
-      // logger.log(`Keys for operator ${NOR_DEVNET_OPERATOR} added`);
+      logger.log(`Increase staking limit for NOR`);
+      await dre.runCommand(LidoSetStakingLimit, { operatorId: 0, limit: 30 });
 
-      // logger.log("Add CSM operator with keys.");
-      // await dre.runCommand("onchain:csm:add-operator", [
-      //   "--name",
-      //   CSM_DEVNET_OPERATOR,
-      // ]);
-      // logger.log(`Keys for operator ${CSM_DEVNET_OPERATOR} added`);
+      logger.log("Add CSM operator with keys.");
+      await dre.runCommand(LidoAddCSMOperatorWithKeys, {
+        name: CSM_DEVNET_OPERATOR,
+      });
+      logger.log(`Keys for operator ${CSM_DEVNET_OPERATOR} added`);
 
-      // logger.log(`Make deposit to NOR`);
-      // await dre.runCommand("onchain:lido:deposit", ["--id", "1"]);
+      logger.log(`Make deposit to NOR`);
+      await dre.runCommand(LidoDeposit, { id: 1, deposits: 30 });
 
-      // logger.log(`Make deposit to CSM`);
-      // await dre.runCommand("onchain:lido:deposit", ["--id", "3"]);
+      logger.log(`Make deposit to NOR`);
+      await dre.runCommand(LidoDeposit, { id: 3, deposits: 30 });
 
-      // logger.log("Add new CSM Verifier");
-      // await dre.runCommand("onchain:csm:add-verifier", args);
+      logger.log(`Adding keys to the validator`)
+      await dre.runCommand(ValidatorAdd, {})
+
+      logger.log("Add new CSM Verifier");
+      await dre.runCommand(DeployCSVerifier, { verify: params.verify });
     }
 
     // Display network information
