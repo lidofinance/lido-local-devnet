9aadf73942689bc277cf00664889415ae9cb9c22
Eddort <webisforce@gmail.com>
2025-03-10 17:09:42 +0100

    feat: add ValidatorList command to list all validator keystores in the system

---
+++ src/commands/validator/list.ts
@@ -0,0 +1,24 @@
+import { command } from "@devnet/command";
+import * as keyManager from "@devnet/key-manager-api";
+
+export const ValidatorList = command.cli({
+  description: "Lists all validator keystores in the system",
+  params: {},
+  async handler({ dre: { logger, state } }) {
+    const { validatorsApi } = await state.getChain();
+    const keystoresResponse = await keyManager.fetchKeystores(
+      validatorsApi,
+      keyManager.KEY_MANAGER_DEFAULT_API_TOKEN,
+    );
+
+    logger.log(`Found ${keystoresResponse.data.length} validator keystores:`);
+
+    if (keystoresResponse.data.length > 0) {
+      keystoresResponse.data.forEach((keystore, index) => {
+        logger.log(`[${index + 1}] ${keystore.validating_pubkey}`);
+      });
+    } else {
+      logger.log("No validator keystores found.");
+    }
+  },
+});
