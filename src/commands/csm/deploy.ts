97742a823d85cd1de660b9ad8325473c00b888be
madlabman <10616301+madlabman@users.noreply.github.com>
2025-03-20 15:25:27 +0100

    chore: use legacy txs for CSM deploy and lower gas multiplier

--- src/commands/csm/deploy.ts
+++ src/commands/csm/deploy.ts
@@ -99,7 +99,7 @@ export const DeployCSMContracts = command.cli({
 
     await dre.runCommand(CSMInstall, {});
 
-    const args = ["deploy-live-no-confirm", "-g", "300"];
+    const args = ["deploy-live-no-confirm", "-g", "200", "--legacy"];
     if (params.verify) {
       args.push("--verify", "--verifier", "blockscout", "--chain", "32382");
     }
