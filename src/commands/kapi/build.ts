a05c59613fad34b8fe5f24f28a93ab0f5789ada4
infloop <infloop.gm@gmail.com>
2025-09-01 16:29:16 +0300

    feat: WIP on docker images pushing to registry

---
+++ src/commands/kapi/build.ts
@@ -0,0 +1,25 @@
+import { command } from "@devnet/command";
+import { buildAndPushDockerImage } from "@devnet/docker";
+
+export const KapiBuild = command.cli({
+  description: "Build Kapi",
+  params: {},
+  async handler({ dre: { state, network, services, logger } }) {
+    const dockerRegistry = await state.getDockerRegistry();
+
+    const TAG = `kt-${network.name}`;
+    const IMAGE = 'keys-api';
+
+    await buildAndPushDockerImage({
+      cwd: services.kapi.artifact.root,
+      registryUrl: dockerRegistry.registryUrl,
+      buildContext: '.',
+      imageName: IMAGE,
+      tag: TAG,
+      password: 'test',
+      username: 'test',
+    })
+
+    logger.log(`Kapi image pushed to ${dockerRegistry.registryUrl}/${IMAGE}:${TAG}`)
+  },
+});
