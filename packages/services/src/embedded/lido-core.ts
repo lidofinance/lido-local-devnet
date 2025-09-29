bc2eac9cb0b4495f19525f4febf5abc79df0ac78
Alexander Movsunov <alex.mov@lido.fi>
2025-09-29 15:42:40 +0400

    fix: network defaults path

--- packages/services/src/embedded/lido-core.ts
+++ packages/services/src/embedded/lido-core.ts
@@ -3,7 +3,7 @@ import { DevnetServiceConfig } from "../devnet-service-config.js";
 export const lidoCore = new DevnetServiceConfig({
   repository: {
     url: "git@github.com:lidofinance/core.git",
-    branch: "develop",
+    branch: "feat/staking-router-3.0",
   },
   name: "lidoCore" as const,
   constants: {
@@ -13,7 +13,7 @@ export const lidoCore = new DevnetServiceConfig({
     GAS_PRIORITY_FEE: "1",
     NETWORK: "local-devnet",
     NETWORK_STATE_DEFAULTS_FILE:
-      "scripts/scratch/deployed-testnet-defaults.json",
+      "scripts/defaults/local-devnet-defaults.json",
     NETWORK_STATE_FILE: `deployed-local-devnet.json`,
     SLOTS_PER_EPOCH: "32",
   },
