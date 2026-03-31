47f3893ad2c78ba6fcb25725e677a024c4c6df32
Alexander Movsunov <alex.mov@lido.fi>
2026-03-31 19:40:18 +0400

    fix: depositor modules allowlist

--- src/commands/dsm-bots-k8s/up.ts
+++ src/commands/dsm-bots-k8s/up.ts
@@ -72,7 +72,7 @@ export const DSMBotsK8sUp = command.cli({
       RABBIT_MQ_USERNAME: "guest",
       RABBIT_MQ_PASSWORD: "guest",
       CREATE_TRANSACTIONS: "true",
-      DEPOSIT_MODULES_WHITELIST: "1\\,2\\,3", // necessary wrapping for helm
+      DEPOSIT_MODULES_WHITELIST: "1\\,2\\,3\\,4", // necessary wrapping for helm
       PROMETHEUS_PREFIX: "depositor_bot",
       KEYS_API_URLS: kapiPrivateUrl,
       CL_API_URLS: clPrivate,
